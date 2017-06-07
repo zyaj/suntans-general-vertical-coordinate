da64a184fd13c0520a34ab0ab721aa725f9f5e03
zyaj <zyaj@stanford.edu>
2017-06-07 12:52:35 -0700

    add stability check example

---
+++ examples/stability-check/uservertcoordinate.c
@@ -0,0 +1,151 @@
+/*
+ * File: uservertcoordinate.c
+ * Author: Yun Zhang
+ * Institution: Stanford University
+ * --------------------------------
+ * This file include a function to user defined vertical coordinate
+ * 
+ */
+
+#include "suntans.h"
+#include "grid.h"
+#include "phys.h"
+#include "initialization.h"
+#include "boundaries.h"
+#include "util.h"
+#include "tvd.h"
+#include "mympi.h"
+#include "scalars.h"
+#include "vertcoordinate.h"
+#include "physio.h"
+#include "subgrid.h"
+
+/*
+ * Function: UserDefinedVerticalCoordinate
+ * User define vertical coordinate 
+ * basically it is a user-defined function to calculate the layer thickness based on 
+ * different criterion
+ * ----------------------------------------------------
+ * the original code has already include 1 z-level, 2 isopycnal, 3 sigma, 4 variational 
+ */
+void UserDefinedVerticalCoordinate(gridT *grid, propT *prop, physT *phys,int myproc)
+{
+  int i,k;
+  for(i=0;i<grid->Nc;i++)
+  { 
+    k=grid->ctop[i];
+    grid->dzz[i][k]=0.7+phys->h[i];
+    for(k=grid->ctop[i]+1;k<10;k++)
+      grid->dzz[i][k]=0.7;
+    for(k=10;k<50;k++)
+      grid->dzz[i][k]=0.05;
+    for(k=50;k<grid->Nk[i];k++)
+      grid->dzz[i][k]=0.7;
+  }
+}
+
+/*
+ * Function: InitializeVerticalCoordinate
+ * to setup the initial condition of dzz for user defined vertical coordinate
+ * ----------------------------------------------------
+ */
+void InitializeVerticalCoordinate(gridT *grid, propT *prop, physT *phys,int myproc)
+{
+  int i,k;
+  for(i=0;i<grid->Nc;i++)
+  { 
+    k=grid->ctop[i];
+    grid->dzz[i][k]=0.7;
+    for(k=grid->ctop[i]+1;k<10;k++)
+      grid->dzz[i][k]=0.7;
+    for(k=10;k<50;k++)
+      grid->dzz[i][k]=0.05;
+    for(k=50;k<grid->Nk[i];k++)
+      grid->dzz[i][k]=0.7;
+  }	
+}
+
+/*
+ * Function: InitializeIsopycnalCoordinate
+ * User define isopycnal coordinate 
+ * define the initial dzz for each cell under isopycnal coordinate
+ * ----------------------------------------------------
+ */
+void InitializeIsopycnalCoordinate(gridT *grid, propT *prop, physT *phys,int myproc)
+{
+   int i,k;
+   REAL ztop,zbot;
+   REAL ci=10, pi=3.141592653589793,H=5000,L=100000;
+
+   for(i=0;i<grid->Nc;i++)
+   {
+      for(k=0;k<grid->Nk[i];k++){
+        grid->dzz[i][k]=500+0.04979*sin(pi/10*(k+1)+4.55525)*cos(grid->xv[i]*pi/L); //good for Nk=10
+        grid->dzzold[i][k]=grid->dzz[i][k];
+        //if(i==0)
+        //  printf("k %d dzz %e\n",k,grid->dzz[i][k] );
+      }
+
+   }
+}
+/*
+ * Function: InitializeVariationalCoordinate
+ * Initialize dzz for variational vertical coordinate
+ * ----------------------------------------------------
+ */
+void InitializeVariationalCoordinate(gridT *grid, propT *prop, physT *phys,int myproc)
+{
+	// one for prop->n==1
+}
+
+/*
+ * Function: UserDefinedSigmaCoordinate
+ * User define sigma coordinate 
+ * basically to define the dsigma for each layer
+ * ----------------------------------------------------
+ */
+void InitializeSigmaCoordinate(gridT *grid, propT *prop, physT *phys, int myproc)
+{
+  int i,k;
+  for(k=0;k<grid->Nkmax-2;k++){
+
+  	vert->dsigma[k]=1.0/grid->Nkmax;
+  }
+  vert->dsigma[grid->Nkmax-1]=1e-12;
+  vert->dsigma[grid->Nkmax-2]=2.0/grid->Nkmax-1e-12;
+
+  for(i=0;i<grid->Nc;i++)
+  {
+  	for(k=grid->ctop[i];k<grid->Nk[i];k++)
+  	{
+  	  grid->dzz[i][k]=vert->dsigma[k]*(grid->dv[i]+phys->h[i]);
+  	  grid->dzzold[i][k]=grid->dzz[i][k];
+    }
+  }
+}
+
+/*
+ * Function: MonitorFunctionForVariationalMethod
+ * calculate the value of monitor function for the variational approach
+ * to update layer thickness when nonlinear==4
+ * ----------------------------------------------------
+ * Mii=sqrt(1-alphaM*(drhodz)^2)
+ */
+void MonitorFunctionForAverageMethod(gridT *grid, propT *prop, physT *phys, int myproc)
+{
+}
+
+/*
+ * Function: MonitorFunctionForVariationalMethod
+ * calculate the value of monitor function for the variational approach
+ * to update layer thickness when nonlinear==4
+ * solve the elliptic equation using iteration method
+ * ----------------------------------------------------
+ * Mii=sqrt(1-alphaM*(drhodz)^2)
+ * alpha_H define how much horizontal diffusion 
+ * alphaH define how much horizontal density gradient 
+ * alphaV define how much vertical density gradient
+ */
+void MonitorFunctionForVariationalMethod(gridT *grid, propT *prop, physT *phys, int myproc, int numprocs, MPI_Comm comm)
+{
+}
